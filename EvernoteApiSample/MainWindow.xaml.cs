075569cdfdfb35c78faf04b244701b950f268a8f
Koichi-Kobayashi <koichi.code@gmail.com>
2015-08-23 17:31:41 +0900

    UI 作成

--- EvernoteApiSample/MainWindow.xaml.cs
+++ EvernoteApiSample/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -38,6 +39,68 @@ namespace EvernoteApiSample
       {
         ENSession.SharedSession.AuthenticateToEvernote();
       }
+    }
+
+    private void Window_Drop(object sender, DragEventArgs e)
+    {
+      FileList list = this.DataContext as FileList;
+      string[] files = e.Data.GetData(DataFormats.FileDrop) as string[];
+      if (files != null)
+      {
+        foreach (var m in 
+                 from s in files 
+                 where s.LastIndexOf(".docx") != -1 || s.LastIndexOf(".xlsx") != -1 || s.LastIndexOf(".pdf") != -1
+                 select s)
+        {
+          list.FileNames.Add(m);
+        }
+        if (listBox.Items.Count > 0)
+        {
+          uploadButton.IsEnabled = true;
+          clearButton.IsEnabled = true;
+        }
+      }
+    }
+
+    private void Window_PreviewDragOver(object sender, DragEventArgs e)
+    {
+      if (e.Data.GetDataPresent(DataFormats.FileDrop, true))
+      {
+        e.Effects = DragDropEffects.Copy;
+      }
+      else
+      {
+        e.Effects = DragDropEffects.None;
+      }
+      e.Handled = true;
+    }
+
+    private void uploadButton_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void clearButton_Click(object sender, RoutedEventArgs e)
+    {
+      FileList list = this.DataContext as FileList;
+      list.clear();
+    }
+  }
+
+  public class FileList
+  {
+    public FileList()
+    {
+      FileNames = new ObservableCollection<string>();
+    }
+    public ObservableCollection<string> FileNames
+    {
+      get;
+      private set;
+    }
+    public void clear()
+    {
+      this.FileNames.Clear();
     }
   }
 }
